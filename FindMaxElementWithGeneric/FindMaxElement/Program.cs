856096848390707e99bf9dd256292fd1a1768137
prashikjaware <prashikjaware123@gmail.com>
2021-03-21 12:01:39 +0530

    [Prashik]Added: Find Maximum of Three Doubles

--- FindMaxElementWithGeneric/FindMaxElement/Program.cs
+++ FindMaxElementWithGeneric/FindMaxElement/Program.cs
@@ -20,12 +20,29 @@ namespace FindMaxElement
                 return first;
         }
 
+        public static double MaximumFloatNumber(double first, double second, double third)
+        {
+            if (first > second && first > third)
+                return first;
+            else if (second > first && second > third)
+                return second;
+            else if (third > first && third > second)
+                return third;
+            else
+                return first;
+        }
+
         static void Main(string[] args)
         {
             int[] integer = { 3, 2, 0 };
             Console.WriteLine("MAXIMUM INTEGER");
             Console.WriteLine(MaximumIntegerNumber(integer[0], integer[1], integer[2]));
             Console.WriteLine("-----------------");
+            double[] floatNumber = { 3.1, 22.5, 0.2 };
+            Console.WriteLine("MAXIMUM DECIMAL");
+            Console.WriteLine(MaximumFloatNumber(floatNumber[0], floatNumber[1], floatNumber[2]));
+            Console.WriteLine("-----------------");
+
             Console.ReadKey();
 
         }
